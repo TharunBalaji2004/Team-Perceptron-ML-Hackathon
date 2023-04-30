badabfaaed8c89dfb73bd1620f4097037afbb737
Nadeem <m.nadeem83005@gmail.com>
2023-04-30 17:07:34 +0530

    flask integrated

--- main.py
+++ main.py
@@ -13,6 +13,33 @@ api = os.getenv("api")
 Flask.secret_key = os.getenv("secret_key")
 
 
+def get_data():
+    my_var = session.get("my_var", None)
+    if my_var is None:
+        my_var = ["Chennai"]
+    list_of_data = requests.get(
+        f"https://api.openweathermap.org/data/2.5/weather?q={my_var[0]}&appid={api}&units=metric"
+    ).json()
+    data = {
+        "cityname": f"{my_var[0]}",
+        "country_code": str(list_of_data["sys"]["country"]),
+        "coordinate": str(list_of_data["coord"]["lon"])
+        + " "
+        + str(list_of_data["coord"]["lat"]),
+        "temp": str(list_of_data["main"]["temp"]),
+        "maxtemp": str(list_of_data["main"]["temp_max"]),
+        "mintemp": str(list_of_data["main"]["temp_min"]),
+        "visibility": str(list_of_data["visibility"] / 1000),
+        "wind": str(list_of_data["wind"]["speed"]),
+        "pressure": str(list_of_data["main"]["pressure"]),
+        "humidity": str(list_of_data["main"]["humidity"]),
+        "weather": str(list_of_data["weather"][0]["description"]),
+        "img" : str(f"https://openweathermap.org/img/wn/{list_of_data['weather'][0]['icon']}@2x.png"),
+        "lastupdate": str(datetime.now()),
+    }
+    return data
+
+
 @app.route("/", methods=["POST", "GET"])
 def home_page():
     if request.method == "POST":
@@ -26,26 +53,13 @@ def home_page():
 
 @app.route("/forecast", methods=["POST", "GET"])
 def forecast():
-    global i
-    i += 1
-    my_var = session.get("my_var", None)
-    if my_var is None:
-        my_var = ["Chennai"]
-    list_of_data = requests.get(
-        f"https://api.openweathermap.org/data/2.5/weather?q={my_var[0]}&appid={api}&units=metric"
-    ).json()
-    data = {
-        "cityname": f"{my_var[0]}",
-        "country_code": str(list_of_data["sys"]["country"]),
-        "coordinate": str(list_of_data["coord"]["lon"])
-        + " "
-        + str(list_of_data["coord"]["lat"]),
-        "temp": str(list_of_data["main"]["temp"]) + " °C",
-        "pressure": str(list_of_data["main"]["pressure"]),
-        "humidity": str(list_of_data["main"]["humidity"]),
-        "weather": str(list_of_data["weather"][0]["main"]),
-        "lastupdate": str(datetime.now()),
-    }
+    if request.method == "POST":
+        city = request.form.get("city")
+        statecode = request.form.get("state")
+        countrycode = "in"
+        session["my_var"] = [city, statecode, countrycode]
+        return redirect(url_for("forecast"))
+    data = get_data()
     print(data)
     return render_template("weather.html", data=data)
 
